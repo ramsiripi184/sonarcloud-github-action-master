fe64488846e1c64bb3d337eac2d36f8d8387902d
SIRIPI VENKATARAMESWARA REDDY <ramsiripi184@gmail.com>
2023-01-25 14:24:22 +0530

    added files

--- Powershell/deleteRG.ps1
+++
@@ -1,8 +0,0 @@
-Param(
-    [parameter(Mandatory = $false)]
-    [string]$resourceGroupName = "demo-ossconf"
-)
-
-# Delete AKS cluster
-Write-Host "Deleting resource group $resourceGroupName" -ForegroundColor Red
-az group delete --name=$resourceGroupName --yes
